218e25e37409bab499c71b7498c4b6da264e4f13
helmi0611 <110041171+helmi0611@users.noreply.github.com>
2022-12-26 21:57:59 +0700

    Edited model

---
+++ IntegerWebApplication/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using IntegerWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace IntegerWebApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            var student = new Student();
+            {
+                student.Id = 1;
+                student.Name = "Helmi Tris Edyan";
+                student.Addres = "Banjar Patroman";
+                student.PhoneNumber = "082117465976";
+            }
+            return View(student);
+        }
+    }
+}

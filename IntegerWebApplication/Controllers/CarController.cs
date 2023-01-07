2f1831c6decb011d1397e6c5d0f9227d1576d19c
helmi0611 <110041171+helmi0611@users.noreply.github.com>
2023-01-07 22:13:28 +0700

    Tugas

--- IntegerWebApplication/Controllers/CarController.cs
+++ IntegerWebApplication/Controllers/CarController.cs
@@ -31,17 +31,37 @@ namespace IntegerWebApplication.Controllers
                 Brand = "Toyota",
                 Varian = "Red"
             });*/
-
-            var cars = new Car[]
-            {
-                new Car{IDRegistration = 1, Type ="Sedan", Brand= "Toyota", Varian="FT86"},
-                new Car{IDRegistration = 2, Type ="SUV", Brand= "Honda", Varian="ACV"},
-                new Car{IDRegistration = 3, Type ="Sedan", Brand= "BMW", Varian="ADX"},
-                new Car{IDRegistration = 4, Type ="SUV", Brand= "Buggati", Varian="Accord"}
+            var cars = new Car[]{
+                new Car{IDRegistration = 1, Type= "Sedan", Brand = "Toyota", Varian = "FT86"},
+                new Car{IDRegistration = 2, Type = "SUV", Brand = "Toyota", Varian = "RAV4"},
+                new Car{IDRegistration = 3, Type = "Sedan", Brand = "Honda", Varian = "Accord"},
+                new Car{IDRegistration = 4, Type = "SUV", Brand = "Honda", Varian = "CRV"},
+                new Car{IDRegistration = 5, Type = "Sedan", Brand = "Honda", Varian = "City"},
             };
+            
+            //No 1
+            var Soal1 = cars.Where(x => x.Brand == "Honda").FirstOrDefault(); //Kalau pake ini harus pakai list yang datanya 1
+            ViewBag.Satu = new List<Car>() {Soal1};
 
-            var car = cars.Where(x => x.Type == "Sedan").FirstOrDefault();
-            ViewBag.Cars = new List<Car>() { car};
+            //No2
+            var Soal2 = cars.Where(x => (x.Brand == "Honda" && x.Type =="Sedan"));
+            ViewBag.Dua = Soal2;
+
+            //
+            var Soal3 = cars.Where(x => (x.Brand == "Honda" && x.Varian =="City")).FirstOrDefault(); 
+            ViewBag.Tiga = new List<Car>() { Soal3 };
+
+            //No4
+            var Soal4 = cars.Where(x => x.Brand == "Toyota");
+            ViewBag.Empat = Soal4;
+
+            //No5
+            var Soal5 = cars.Where(x => x.Varian == "City");
+            ViewBag.Lima = Soal5;
+
+            //No6
+            var Soal6 = cars.Where(x => x.Brand == "Honda");
+            ViewBag.Enam = Soal6;
 
             return View();
         }
